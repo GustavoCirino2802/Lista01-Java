248ce2893beac2f7b7e1ff7dfee9153e66b00329
Auto Logon <autologon@CSED.LAB>
2024-03-14 09:27:49 -0300

    Exercicio05

--- Exercicio05.java
+++ Exercicio05.java
@@ -1,3 +1,18 @@
 public class Exercicio05 {
+    public static void executar() {
 
+    double A = Prompt.lerDecimal("Digite um número A: ");
+    double B = Prompt.lerDecimal("Digite um número B: ");
+
+    double aux;
+
+    aux = A;
+    A = B;
+    B = aux;
+
+    Prompt.imprimir("Original A = " + B);
+    Prompt.imprimir("Trocado A = " + A);
+    Prompt.imprimir("Original B = " + A);
+    Prompt.imprimir("Trocado B = " + B);
+    }
 }
