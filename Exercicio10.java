1cf7c6d5e37c35f77e724a8841dc0603f5d5c424
Auto Logon <autologon@CSED.LAB>
2024-03-14 10:25:26 -0300

    Exercicio10

---
+++ Exercicio10.java
@@ -0,0 +1,13 @@
+public class Exercicio10 {
+    public static void executar() {
+
+        int num = Prompt.lerInteiro("Digite um número: ");
+    
+        if (num == 1) {
+            Prompt.imprimir("UM");
+        }
+        if (num == 2) {
+            
+        }
+    }
+}
