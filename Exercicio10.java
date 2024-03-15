9a8b35f09cc171365f0fd77980e3a9a1719408d0
GustavoCirino2802 <142018650+GustavoCirino2802@users.noreply.github.com>
2024-03-15 12:18:45 -0300

    Exercicio10

--- Exercicio10.java
+++ Exercicio10.java
@@ -1,13 +1,29 @@
 public class Exercicio10 {
     public static void executar() {
 
-        int num = Prompt.lerInteiro("Digite um número: ");
-    
-        if (num == 1) {
-            Prompt.imprimir("UM");
+        int num = Prompt.lerInteiro("Digite um número de 1 a 5: ");
+
+        if (num < 1 || num > 5) {
+            Prompt.imprimir("Número inválido!");         
         }
-        if (num == 2) {
-            
+
+        switch (num) {
+            case 1: 
+                Prompt.imprimir("UM!");
+                break;
+            case 2: 
+                Prompt.imprimir("DOIS!");
+                break;
+            case 3: 
+                Prompt.imprimir("TRÊS!");
+                break;
+            case 4: 
+                Prompt.imprimir("QUATRO!");
+                break;
+            case 5: 
+                Prompt.imprimir("CINCO!");
+                break;
+                
         }
     }
 }
